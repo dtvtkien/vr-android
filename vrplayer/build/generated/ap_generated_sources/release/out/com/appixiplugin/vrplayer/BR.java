e8f025c9a19c5298e36f267f72c1fc54449bd753
dtvtkien <dtvtdevelopersk58@gmail.com>
2020-11-19 14:30:40 +0700

    - Upload bintray 1.0.2

---
+++ vrplayer/build/generated/ap_generated_sources/release/out/com/appixiplugin/vrplayer/BR.java
@@ -0,0 +1,9 @@
+package com.appixiplugin.vrplayer;
+
+public class BR {
+  public static int _all = 0;
+
+  public static int activity = 1;
+
+  public static int mediaPlayer = 2;
+}
---
+++ vrplayer/build/generated/ap_generated_sources/release/out/com/appixiplugin/vrplayer/databinding/ActivityVrPlayerBindingImpl.java
@@ -0,0 +1,100 @@
+package com.appixiplugin.vrplayer.databinding;
+import com.appixiplugin.vrplayer.R;
+import com.appixiplugin.vrplayer.BR;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import android.view.View;
+@SuppressWarnings("unchecked")
+public class ActivityVrPlayerBindingImpl extends ActivityVrPlayerBinding  {
+
+    @Nullable
+    private static final androidx.databinding.ViewDataBinding.IncludedLayouts sIncludes;
+    @Nullable
+    private static final android.util.SparseIntArray sViewsWithIds;
+    static {
+        sIncludes = null;
+        sViewsWithIds = new android.util.SparseIntArray();
+        sViewsWithIds.put(R.id.player, 1);
+    }
+    // views
+    @NonNull
+    private final android.widget.FrameLayout mboundView0;
+    // variables
+    // values
+    // listeners
+    // Inverse Binding Event Handlers
+
+    public ActivityVrPlayerBindingImpl(@Nullable androidx.databinding.DataBindingComponent bindingComponent, @NonNull View root) {
+        this(bindingComponent, root, mapBindings(bindingComponent, root, 2, sIncludes, sViewsWithIds));
+    }
+    private ActivityVrPlayerBindingImpl(androidx.databinding.DataBindingComponent bindingComponent, View root, Object[] bindings) {
+        super(bindingComponent, root, 0
+            , (com.appixiplugin.vrplayer.vr.VrPlayerView) bindings[1]
+            );
+        this.mboundView0 = (android.widget.FrameLayout) bindings[0];
+        this.mboundView0.setTag(null);
+        setRootTag(root);
+        // listeners
+        invalidateAll();
+    }
+
+    @Override
+    public void invalidateAll() {
+        synchronized(this) {
+                mDirtyFlags = 0x2L;
+        }
+        requestRebind();
+    }
+
+    @Override
+    public boolean hasPendingBindings() {
+        synchronized(this) {
+            if (mDirtyFlags != 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean setVariable(int variableId, @Nullable Object variable)  {
+        boolean variableSet = true;
+        if (BR.activity == variableId) {
+            setActivity((com.appixiplugin.vrplayer.presentation.VrPlayerActivity) variable);
+        }
+        else {
+            variableSet = false;
+        }
+            return variableSet;
+    }
+
+    public void setActivity(@Nullable com.appixiplugin.vrplayer.presentation.VrPlayerActivity Activity) {
+        this.mActivity = Activity;
+    }
+
+    @Override
+    protected boolean onFieldChange(int localFieldId, Object object, int fieldId) {
+        switch (localFieldId) {
+        }
+        return false;
+    }
+
+    @Override
+    protected void executeBindings() {
+        long dirtyFlags = 0;
+        synchronized(this) {
+            dirtyFlags = mDirtyFlags;
+            mDirtyFlags = 0;
+        }
+        // batch finished
+    }
+    // Listener Stub Implementations
+    // callback impls
+    // dirty flag
+    private  long mDirtyFlags = 0xffffffffffffffffL;
+    /* flag mapping
+        flag 0 (0x1L): activity
+        flag 1 (0x2L): null
+    flag mapping end*/
+    //end
+}
